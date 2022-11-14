bc4ffb60a7c407305e5103d7edbb5d5ca4e9940f
Alexander Rath <alex@ist.besonders.cool>
2022-11-14 10:33:52 +0100

    Sheet 02

---
+++ rt/solids/solid.cpp
@@ -0,0 +1,13 @@
+#include <rt/solids/solid.h>
+
+namespace rt {
+
+Solid::Solid(CoordMapper* texMapper, Material* material)
+{
+    CG_UNUSED(texMapper);
+    CG_UNUSED(material);
+
+    /* TODO */
+}
+
+}
