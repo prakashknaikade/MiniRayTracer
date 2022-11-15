cb89a657a543ef7f69917837703a8e254630e8fc
Hamza Mughal <mhamza.mughal09@gmail.com>
2022-11-15 20:14:19 +0100

    fix var assign

--- rt/cameras/perspective.cpp
+++ rt/cameras/perspective.cpp
@@ -6,25 +6,25 @@ namespace rt {
 PerspectiveCamera::PerspectiveCamera(const Point& center, const Vector& forward, const Vector& up, float verticalOpeningAngle, float horizontalOpeningAngle)
 {
     /* TODO */
-    _center = center;
+    PerspectiveCamera::_center = center;
   	// PerspectiveCamera::forward = forward;
   	// PerspectiveCamera::up = up;
     // PerspectiveCamera::verticalOpeningAngle = verticalOpeningAngle;
     // PerspectiveCamera::horizontalOpeningAngle = horizontalOpeningAngle;
 
-    cutX = tan(horizontalOpeningAngle/2.0f);
-    cutY = tan(verticalOpeningAngle/2.0f);
+    PerspectiveCamera::cutX = tan(horizontalOpeningAngle/2.0f);
+    PerspectiveCamera::cutY = tan(verticalOpeningAngle/2.0f);
 
-    CamZAxis = forward.normalize();
-    CamXAxis = cross(forward, up.normalize()).normalize();
-    CamYAxis = cross(CamXAxis, CamZAxis).normalize();
+    PerspectiveCamera::CamZAxis = forward.normalize();
+    PerspectiveCamera::CamXAxis = cross(forward, up.normalize()).normalize();
+    PerspectiveCamera::CamYAxis = cross(CamXAxis, CamZAxis).normalize();
 
 }
 
 Ray PerspectiveCamera::getPrimaryRay(float x, float y) const {
     /* TODO */ //NOT_IMPLEMENTED;
-    Vector dir = (x * CamXAxis * cutX + y * CamYAxis * cutY +  CamZAxis).normalize();
-	  return Ray(_center, dir);
+    Vector dir = (x * PerspectiveCamera::CamXAxis * PerspectiveCamera::cutX + y * PerspectiveCamera::CamYAxis * PerspectiveCamera::cutY +  PerspectiveCamera::CamZAxis).normalize();
+	  return Ray(PerspectiveCamera::_center, dir);
 }
 	
 }
