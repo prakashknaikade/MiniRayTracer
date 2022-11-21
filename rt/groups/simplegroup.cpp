d7543d08b283deed7239a7a7ddb728de99229160
Prakash <prakashknaikade@gmail.com>
2022-11-21 08:47:22 +0100

    fix simplegroup

--- rt/groups/simplegroup.cpp
+++ rt/groups/simplegroup.cpp
@@ -39,7 +39,8 @@ void SimpleGroup::add(Primitive* p) {
 
 void SimpleGroup::setMaterial(Material* m) {
     // /* TODO */ NOT_IMPLEMENTED;
-    this->setMaterial(m);
+    for (int i = 0; i < this->primitives.size(); i++) {
+        this->primitives[i]->setMaterial(m);
 }
 
 void SimpleGroup::setCoordMapper(CoordMapper* cm) {
