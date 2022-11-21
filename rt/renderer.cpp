14ed209b5053d01163b45c5a83adee8be041a3e7
Hamza Mughal <mhamza.mughal09@gmail.com>
2022-11-21 06:38:28 +0100

    add rendering code

--- rt/renderer.cpp
+++ rt/renderer.cpp
@@ -4,6 +4,7 @@
 #include <rt/ray.h>
 #include <iostream>
 #include <rt/cameras/camera.h>
+#include "integrators/integrator.h"
 
 namespace rt {
 
@@ -13,7 +14,22 @@ Renderer::Renderer(Camera* cam, Integrator* integrator)
 
 void Renderer::render(Image& img) {
     CG_UNUSED(img);
-    /* TODO */ NOT_IMPLEMENTED;
+    // /* TODO */ NOT_IMPLEMENTED;
+    for (uint y = 0; y < img.height(); ++y)
+		{
+			for (uint x = 0; x < img.width(); ++x)
+			{
+				float cameraX = 2.0 * float(x + 0.5) / float(img.width()) - 1;
+				float cameraY = 1 - 2.0 * float(y + 0.5) / float(img.height());
+
+				Ray r = cam->getPrimaryRay(cameraX, cameraY);
+
+				RGBColor color = integrator->getRadiance(r);
+				
+				RGBColor* pixelColor = &img(x, y);
+				*pixelColor = color; // * 255;
+			}
+		}
 }
 
 }
