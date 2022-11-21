1cff2f63927bddd183ecf1445546512297e5c7d9
Prakash <prakashknaikade@gmail.com>
2022-11-21 03:16:59 +0100

    implement integrator

--- rt/integrators/casting.cpp
+++ rt/integrators/casting.cpp
@@ -1,9 +1,19 @@
 #include <rt/integrators/casting.h>
+#include <rt/intersection.h>
+#include <rt/world.h>
 
 namespace rt {
 
 RGBColor RayCastingIntegrator::getRadiance(const Ray& ray) const {
-    /* TODO */ NOT_IMPLEMENTED;
+    /* TODO */ //NOT_IMPLEMENTED;
+    Intersection intersection = world->scene->intersect(ray);
+    if(intersection) {
+        float grey_value = -1.0f * dot(ray.d.normalize(), intersection.normal());
+        return RGBColor::rep(grey_value).clamp();
+    }
+    else{
+        return RGBColor(0,0,0);
+    }
 }
 
 }
