4a32f9faaba8785d80aac8324b0f28fb4dcb1c26
Hamza Mughal <mhamza.mughal09@gmail.com>
2022-11-21 05:29:52 +0100

    fix code for groups

--- rt/groups/simplegroup.cpp
+++ rt/groups/simplegroup.cpp
@@ -8,23 +8,41 @@ BBox SimpleGroup::getBounds() const {
 
 Intersection SimpleGroup::intersect(const Ray& ray, float tmin, float tmax) const {
     /* TODO */ //NOT_IMPLEMENTED;
-    Intersection _intersection;
-    bool status_found = false;
+    // Intersection _intersection;
+    // bool status_found = false;
 
-    for(std::vector<Primitive *>::size_type i = 0; i != this->primitives.size(); i++) {
-      Intersection intersection = primitives[i]->intersect(ray, tmax);
-      float distance = intersection.distance;
+    // for(std::vector<Primitive *>::size_type i = 0; i != this->primitives.size(); i++) {
+    //   Intersection intersection = primitives[i]->intersect(ray, tmax);
+    //   float distance = intersection.distance;
 
-      if (intersection && (distance < tmax)){
-          tmax = distance;
-          _intersection = intersection;
-          status_found = true;
-      }
-    }
-    if (status_found)
-        return _intersection;
-    else
-        return Intersection::failure();
+    //   if (intersection && (distance < tmax)){
+    //       tmax = distance;
+    //       _intersection = intersection;
+    //       status_found = true;
+    //   }
+    // }
+    // if (status_found)
+    //     return _intersection;
+    // else
+    //     return Intersection::failure();
+    Intersection isec;
+
+	bool anyIntersection = false;
+	for (std::vector<Primitive*>::const_iterator it = primitives.begin(); it != primitives.end(); ++it)
+	{
+		Primitive* p = *it;
+		Intersection newIsec = p->intersect(ray, tmax);
+
+		if (newIsec)
+		{
+			tmax = newIsec.distance;
+			isec = newIsec;
+			anyIntersection = true;
+		}
+	}
+
+	if (anyIntersection) return isec;
+	return Intersection::failure();
 }
 
 
