a1bcd7019602e332dfd6cb2efe95d9ff72d64ad3
Alexander Rath <alex@ist.besonders.cool>
2023-01-04 13:16:09 +0100

    Sheet 06

--- main/main.cpp
+++ main/main.cpp
@@ -13,7 +13,8 @@ void a_smooth();
 void a_bumpmappers();
 
 int main(int /*argc*/, char** /*argv*/) {
-    a_materials();
-    a_lighting();
+    a_textures();
+    a_local();
+    a_mappers();
     return 0;
 }
