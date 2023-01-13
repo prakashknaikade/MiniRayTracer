12b1febdc0cd4756159e4f52fc009f33b18b759c
Hamza Mughal <mhamza.mughal09@gmail.com>
2023-01-13 02:29:16 +0100

    add texture

--- rt/textures/constant.cpp
+++ rt/textures/constant.cpp
@@ -9,23 +9,27 @@ ConstantTexture::ConstantTexture()
 
 ConstantTexture::ConstantTexture(const RGBColor& color)
 {
-    CG_UNUSED(color);
+    // CG_UNUSED(color);
     /* TODO */
+     this->const_color = color;
 }
 
 RGBColor ConstantTexture::getColor(const Point& coord) {
     CG_UNUSED(coord);
-    /* TODO */ NOT_IMPLEMENTED;
+    // /* TODO */ NOT_IMPLEMENTED;
+    return const_color;
 }
 
 RGBColor ConstantTexture::getColorDX(const Point& coord) {
     CG_UNUSED(coord);
-    /* TODO */ NOT_IMPLEMENTED;
+    // /* TODO */ NOT_IMPLEMENTED;
+    return RGBColor::rep(0.0f);
 }
 
 RGBColor ConstantTexture::getColorDY(const Point& coord) {
     CG_UNUSED(coord);
-    /* TODO */ NOT_IMPLEMENTED;
+    // /* TODO */ NOT_IMPLEMENTED;
+    return RGBColor::rep(0.0f);
 }
 
 }
