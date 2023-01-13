a4d2e1ebbf327be8bbd42679e72c05d3dc8d99ca
Hamza Mughal <mhamza.mughal09@gmail.com>
2023-01-13 22:08:50 +0100

    assign 5 main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -13,8 +13,7 @@ void a_smooth();
 void a_bumpmappers();
 
 int main(int /*argc*/, char** /*argv*/) {
-    a_textures();
-    a_local();
-    a_mappers();
+    a_materials();
+    a_lighting();
     return 0;
 }
